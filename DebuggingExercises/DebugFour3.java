dd0f8ab9bea2cacbb4b1021389af229afdfa2e4f
jc431075 <kyle.vincent@my.jcu.edu.au>
2017-08-17 15:57:41 +1000

    working on debugging

--- DebuggingExercises/DebugFour3.java
+++ DebuggingExercises/DebugFour3.java
@@ -3,17 +3,15 @@ public class DebugFour3
 {
    public static void main(String args[])
    {
-      int width = 12,
-      length = 10,
-      height = 8;
-      FixDebugBox box1 = new DebugBox();
-      FixDebugBox box2 = new DebugBox(width, length, height);
+      int width = 12, length = 10, height = 8;
+      DebugBox box1 = new DebugBox();
+      DebugBox box2 = new DebugBox(width, length, height);
       System.out.println("The dimensions of the first box are");
-      showData();
+      DebugBox.showData(width, length, height);
       System.out.print("  The volume of the first box is ");
-      box1.showVolume();
+      box1 = showVolume();
       System.out.println("The dimensions of the second box are");
-      box2.showData();
+      box2 = showData();
       System.out.print("  The volume of the second box is ");
       box2.showVolume();
    }
